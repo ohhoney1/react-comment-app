979f2dd32d5443a328030d4fe7f247d93bf6c6f8
ohhoney1 <1269075501@qq.com>
2018-08-09 11:20:14 +0800

    add demand: save username in localStorage & auto blur on textarea when loads page

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
+const USERNAME = '__comment-app-username__';
+
 class CommentInput extends Component {
+  // static propTypes = {
+  //   onAddComment: PropTypes.func
+  // }
+
   constructor() {
     super();
     this.state = {
@@ -9,14 +15,27 @@ class CommentInput extends Component {
     };
 
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleUsernameBlur = this.handleUsernameBlur.bind(this);
     this.handleCommentChange = this.handleCommentChange.bind(this);
     this.handleComment = this.handleComment.bind(this);
+  }
+
+  componentWillMount() {
+    this._loadUsername();
+  }
+
+  componentDidMount() {
+    if (this.state.username) this.textarea.focus();
   }
 
   handleUsernameChange(evt) {
     this.setState({
       username: evt.target.value
     });
+  }
+
+  handleUsernameBlur() {
+    this._saveUsername();
   }
 
   handleCommentChange(evt) {
@@ -29,6 +48,15 @@ class CommentInput extends Component {
     this.props.onAddComment(this.state);
   }
 
+  _saveUsername() {
+    localStorage.setItem(USERNAME, this.state.username);
+  }
+
+  _loadUsername() {
+    const username = localStorage.getItem(USERNAME) || '';
+    this.setState({ username });
+  }
+
   render() {
     const { username, comment } = this.state;
 
@@ -36,11 +64,20 @@ class CommentInput extends Component {
       <div className='comment-input-wrap border'>
         <div className='username'>
           <label>用户名：</label>
-          <input type='text' className='border' value={username} onChange={this.handleUsernameChange} />
+          <input
+            type='text'
+            className='border'
+            value={username}
+            onChange={this.handleUsernameChange}
+            onBlur={this.handleUsernameBlur} />
         </div>
         <div className='comment'>
           <label>评论内容：</label>
-          <textarea value={comment} className='border' onChange={this.handleCommentChange} />
+          <textarea
+            className='border'
+            ref={textarea => this.textarea = textarea}
+            value={comment}
+            onChange={this.handleCommentChange} />
         </div>
         <div className='release clearfix'>
           <button className='fr border' onClick={this.handleComment}>发布</button>
